7516273d76535051d420c6476981231d5cd45ed8
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-11-25 21:55:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+pip install plotly
+streamlit run stock_dashboard.py
+
 
 df = pd.read_csv("project1data.csv")
 st.dataframe(df)
