7649896ffaeb5a46b27fa2f4eb6cc9d0f7b7f6a9
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-11-28 11:17:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,15 +93,6 @@ try:
     # Additional analysis or visualizations can be added here
     #showing chosen symbol with chosen stock on same axis
 
-chart_data = pd.DataFrame(
-   {
-       "col1": np.random.randn(20),
-       "col2": np.random.randn(20),
-       "col3": np.random.choice(["A", "B", "C"], 20),
-   }
-)
-
-st.line_chart(chart_data, x="col1", y="col2", color="col3")
 
 
 except Exception as e:
